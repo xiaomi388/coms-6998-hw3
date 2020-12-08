7cf11c4ce45b625675a3c83f20a806cbcf586f91
xiaomi388 <xiaomi388@gmail.com>
2020-12-08 17:04:43 +0800

    tweak

--- upload.sh
+++ upload.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-OLDPWD=$(dirname "$0")
-
-mkdir -p ./package
 # shellcheck disable=SC2164
-cd "${OLDPWD}/package"
-zip -r9 "${OLDPWD}/function.zip" .
+cd "$1/package"
+zip -r "${OLDPWD}/function.zip" .
 
-# shellcheck disable=SC2164
-cd "${OLDPWD}"
+cd ..
 ls
 zip -g function.zip index.py
 aws lambda update-function-code --function-name "$LF1" --zip-file fileb://function.zip --region "$AWS_DEFAULT_REGION"
