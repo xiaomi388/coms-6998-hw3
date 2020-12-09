ee4f550987cf4cbabf80a4fcec90b4c73287c157
xiaomi388 <xiaomi388@gmail.com>
2020-12-09 15:03:52 +0800

    fix bug in the upload script

--- upload.sh
+++ upload.sh
@@ -7,4 +7,4 @@ zip -r "../function.zip" .
 cd ..
 cat index.py
 zip -g function.zip index.py
-aws lambda update-function-code --function-name "$LF1" --zip-file fileb://function.zip --region "$AWS_DEFAULT_REGION"
+aws lambda update-function-code --function-name "$2" --zip-file fileb://function.zip --region "$AWS_DEFAULT_REGION"
