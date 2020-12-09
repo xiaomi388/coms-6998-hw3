a7fb00cf0d896743477a6f6fca7a9aa0ad25fda5
xiaomi388 <xiaomi388@gmail.com>
2020-12-09 14:52:16 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -5,5 +5,6 @@ cd "$1/package"
 zip -r "../function.zip" .
 
 cd ..
+cat index.py
 zip -g function.zip index.py
 aws lambda update-function-code --function-name "$LF1" --zip-file fileb://function.zip --region "$AWS_DEFAULT_REGION"
