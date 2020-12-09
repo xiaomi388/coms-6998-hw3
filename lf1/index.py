76389248b968c585325ffe9fe299b746b10d4dfe
Aishan Liu <aishanliu0505@163.com>
2020-12-09 00:17:27 -0500

    first lf1 update

--- lf1/index.py
+++ lf1/index.py
@@ -2,7 +2,8 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    print("Event: ", event)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
-    }
+   } 
