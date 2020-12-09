84a4cda5d06faf62134cdca9d80dc2f6bb8cb4a6
violet23 <aishanliu0505@163.com>
2020-12-09 01:07:52 -0500

    Update index.py

--- lf1/index.py
+++ lf1/index.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("Event: ", event)
+    print("Here, look at the event: ", event)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
